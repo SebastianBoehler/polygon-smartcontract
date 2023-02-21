6c96d2c9e0a940461c209e76c1d33f27264ca790
Sebastian_B <basti.boehler@hotmail.de>
2023-02-21 18:13:38 +0100

    fix: downgrade ethers version to fix rpc error

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
-const ethers = require('ethers');
-const PRIVATE_KEY = ethers.Wallet.fromPhrase(process.env.PHRASE).privateKey;
+
+const { API_URL, PRIVATE_KEY, POLYGONSCAN_API_KEY } = process.env;
+console.log()
 
 module.exports = {
   defaultNetwork: "polygon_mumbai",
@@ -10,12 +11,12 @@ module.exports = {
     hardhat: {
     },
     polygon_mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
-      accounts: [PRIVATE_KEY]
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
+    apiKey: POLYGONSCAN_API_KEY
   },
   solidity: {
     version: "0.8.9",
